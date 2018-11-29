3b66a8fea5bdee82193c057633f5a043fd00377a
Jacob Zimmerman (jczimm) <jczimm@jczimm.com>
2018-11-29 00:42:12 -0500

    initial raudio demo

--- examples/simple_graph/simple_graph.js
+++ examples/simple_graph/simple_graph.js
@@ -80,7 +80,6 @@ Drawing.SimpleGraph = function(options) {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-
     camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 1000000);
     camera.position.z = 10000;
 
@@ -116,6 +115,7 @@ Drawing.SimpleGraph = function(options) {
         selected: function(obj) {
           // display info
           if(obj !== null) {
+            console.log(obj);
             info_text.select = "Object " + obj.id;
           } else {
             delete info_text.select;
@@ -154,33 +154,54 @@ Drawing.SimpleGraph = function(options) {
    */
   function createGraph() {
 
-    var node = new GRAPHVIS.Node(0);
-    node.data.title = "This is node " + node.id;
-    graph.addNode(node);
-    drawNode(node);
-
-    var nodes = [];
-    nodes.push(node);
-
-    var steps = 1;
-    while(nodes.length !== 0 && steps < that.nodes_count) {
-      node = nodes.shift();
-
-      var numEdges = randomFromTo(1, that.edges_count);
-      for(var i=1; i <= numEdges; i++) {
-        var target_node = new GRAPHVIS.Node(i*steps);
-        if(graph.addNode(target_node)) {
-          target_node.data.title = "This is node " + target_node.id;
-
-          drawNode(target_node);
-          nodes.push(target_node);
-          if(graph.addEdge(node, target_node)) {
-            drawEdge(node, target_node);
-          }
-        }
+    // TEMP: hard-coding data for testing
+    // from https://codepen.io/jczimm/pen/VBBZOz
+    const bucket = {"tracks":[{"trackId":480700689},{"trackId":359978594},{"trackId":356671463},{"trackId":356272697},{"trackId":344049175,"tags":["electronic","ambient","experimental","hiphopbeat"]},{"trackId":185501964,"tags":["electronic","ambient","chill"]},{"trackId":340786932},{"trackId":340014346,"tags":["electronic","rap","experimental"]},{"trackId":340864271},{"trackId":338882196},{"trackId":340850433},{"trackId":340858419},{"trackId":339810494},{"trackId":340187504,"tags":["funky","future"]},{"trackId":340158750,"tags":["electronic"]},{"trackId":340004927},{"trackId":337445405},{"trackId":340028180},{"trackId":339848591},{"trackId":339804837,"tags":["rap","electronic"]},{"trackId":339452530},{"trackId":339712633,"tags":["electronic","house","future"]},{"trackId":337440468,"tags":["electronic","remix"]},{"trackId":343562073},{"trackId":337864490,"tags":["electronic"]},{"trackId":320515013,"tags":["experimental","electronic","chill"]},{"trackId":307741754,"tags":["ambient","electronic"]},{"trackId":310703664,"tags":["electronic","future"]},{"trackId":305079285,"tags":["experimental"]},{"trackId":303601544},{"trackId":303504889,"tags":["funky","chill"]},{"trackId":300196390,"tags":["ambient","electronic"]},{"trackId":220929372,"tags":["electronic","trap"],"note":"hype but chill"},{"trackId":211483614,"tags":["ambient","electronic"]},{"trackId":291958088,"tags":["electronic","ambient","experimental","chill"]},{"trackId":257091221,"tags":["electronic","ambient","chill"],"note":"has a nice, heavy beat"},{"trackId":297755853,"tags":["experimental","electronic","chill","funky"]},{"trackId":296685652,"tags":["electronic","rap"]},{"trackId":214698107},{"trackId":270011377,"tags":["electronic","experimental","chill"]},{"trackId":292125528,"tags":["electronic","ambient","chill"]},{"trackId":299510194,"tags":["electronic","experimental","remix"]},{"trackId":298434286,"tags":["electronic","funky","remix"]},{"trackId":264628389,"tags":["ambient","chill"]},{"trackId":297297656},{"trackId":296502414,"tags":["ambient","electronic","chill"]},{"trackId":297156837,"tags":["electronic","ambient","experimental"]},{"trackId":297455260},{"trackId":292618548,"tags":["electronic","chill"]},{"trackId":295592367,"tags":["electronic","future","trap"]},{"trackId":296087000,"tags":["electronic"]},{"trackId":295165549,"tags":["electronic","funky","deep"]},{"trackId":295165352,"tags":["electronic","chill"]},{"trackId":294489171,"tags":["electronic","experimental","trap"]},{"trackId":295161513,"tags":["electronic","future","chill"]},{"trackId":227042825,"tags":["electronic","ambient","chill"]},{"trackId":246195391,"tags":["electronic"]},{"trackId":274569356,"tags":["remix"]},{"trackId":281670564,"tags":["remix","electronic"]},{"trackId":281024707,"tags":["electronic","deep","house","remix"]},{"trackId":294901395,"tags":["chill","electronic"]},{"trackId":294279693,"tags":["electronic","experimental","remix"]},{"trackId":284887739,"tags":["electronic","ambient","future","chill","experimental","remix"]},{"trackId":294231299,"tags":["electronic","experimental"]},{"trackId":293351192},{"trackId":293196567,"tags":["electronic","trap"]},{"trackId":294164740,"tags":["electronic","ambient","cover"]},{"trackId":294164678,"tags":["electronic","funky","experimental"]},{"trackId":188441762,"tags":["electronic","experimental","ambient"]},{"trackId":293659186,"tags":["electronic","ambient"]},{"trackId":293969465,"tags":["electronic","chill"]},{"trackId":293585764,"tags":["electronic","experimental","chill"]},{"trackId":267315572},{"trackId":285435997,"tags":["electronic","chill"]},{"trackId":228024161,"tags":["remix","electronic","chill"]},{"trackId":283523608,"tags":["electronic","future","funky"]},{"trackId":285853076,"tags":["electronic","future","chill"]},{"trackId":285709322,"tags":["electronic","remix","experimental"]},{"trackId":284266335,"tags":[]},{"trackId":283814781,"tags":["electronic","ambient","chill"]},{"trackId":245940906,"tags":["ambient","electronic"]},{"trackId":283852262},{"trackId":266399001},{"trackId":283052283},{"trackId":283083300},{"trackId":248426231},{"trackId":282734118},{"trackId":281672945},{"trackId":271291165},{"trackId":281674269},{"trackId":278404934},{"trackId":278366594},{"trackId":271409580},{"trackId":271256313}],"tags":["electronic","ambient","experimental","future","funky","trap","chill","rap","deep","remix","house","cover","hiphopbeat"]};
+    const trackNodes = [];
+    let trackNodeId;
+    const edges = [];
+    bucket.tracks.forEach(({ trackId, tags }) => {
+      if (tags && tags.length) {
+        trackNodeId = `T${trackId}`;
+        trackNodes.push({
+          id: trackNodeId,
+          label: '',
+          group: 'tracks',
+        });
+        tags.forEach(tag => {
+          edges.push({
+            from: trackNodeId,
+            to: `C${tag}`,
+          });
+        });
       }
-      steps++;
-    }
+    });
+
+    const tagNodes = bucket.tags.map(tag => ({
+      id: `C${tag}`,
+      label: tag,
+      group: 'tags',
+    }));
+
+    const nodes = trackNodes.concat(tagNodes);
+
+    // render the nodes and edges based on the arrays of node and edge data
+    const graphVisNodes = {};
+    let newNode;
+    nodes.forEach((node) => {
+      newNode = new GRAPHVIS.Node(node.id);
+      if (node.label) newNode.data.title = node.label;
+      newNode.data.group = node.group;
+
+      if (graph.addNode(newNode)) drawNode(newNode);
+      graphVisNodes[newNode.id] = newNode;
+    });
+    let fromNode, toNode;
+    edges.forEach((edge) => {
+      fromNode = graphVisNodes[edge.from];
+      toNode = graphVisNodes[edge.to];
+      if (graph.addEdge(fromNode, toNode)) drawEdge(fromNode, toNode);
+    });
 
     that.layout_options.width = that.layout_options.width || 2000;
     that.layout_options.height = that.layout_options.height || 2000;
@@ -192,12 +213,17 @@ Drawing.SimpleGraph = function(options) {
     info_text.edges = "Edges " + graph.edges.length;
   }
 
-
   /**
    *  Create a node object and add it to the scene.
    */
   function drawNode(node) {
-    var draw_object = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( {  color: Math.random() * 0xe0e0e0, opacity: 0.8 } ) );
+    // also hard-coded TMP
+    const nodeColors = {
+      tracks: 0xcd5c5c, // raudio red
+      tags: 0x97ba28 // raudio green
+    };
+
+    var draw_object = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( {  color: nodeColors[node.data.group], opacity: 0.8 } ) );
     var label_object;
 
     if(that.show_labels) {
@@ -229,7 +255,7 @@ Drawing.SimpleGraph = function(options) {
    *  Create an edge object (line) and add it to the scene.
    */
   function drawEdge(source, target) {
-      material = new THREE.LineBasicMaterial({ color: 0x606060 });
+      material = new THREE.LineBasicMaterial({ color: 0x777777 });
 
       var tmp_geo = new THREE.Geometry();
       tmp_geo.vertices.push(source.data.draw_object.position);
